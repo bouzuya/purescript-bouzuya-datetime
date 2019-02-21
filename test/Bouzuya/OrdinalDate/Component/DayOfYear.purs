69b9ee780e9ba4c930e2d2cec06af0d550464121
bouzuya <m@bouzuya.net>
2019-02-21 16:09:14 +0900

    :art:

--- test/Bouzuya/OrdinalDate/Component/DayOfYear.purs
+++ test/Bouzuya/OrdinalDate/Component/DayOfYear.purs
@@ -1,9 +1,11 @@
-module Test.Bouzuya.OrdinalDate.Component.DayOfYear (tests) where
+module Test.Bouzuya.OrdinalDate.Component.DayOfYear
+  ( tests
+  ) where
 
-import Bouzuya.OrdinalDate.Component.DayOfYear (firstDayOfYear, lastDayOfYear)
-import Data.Enum (toEnum)
-import Data.Maybe (fromJust)
-import Partial.Unsafe (unsafePartial)
+import Bouzuya.OrdinalDate.Component.DayOfYear as DayOfYear
+import Data.Enum as Enum
+import Data.Maybe as Maybe
+import Partial.Unsafe as Unsafe
 import Prelude (discard)
 import Test.Unit (TestSuite, suite, test)
 import Test.Unit.Assert as Assert
@@ -11,13 +13,13 @@ import Test.Unit.Assert as Assert
 tests :: TestSuite
 tests = suite "Bouzuya.OrdinalDate.Component.DayOfYear" do
   let
-    year2018 = (unsafePartial (fromJust (toEnum 2018)))
-    year2020 = (unsafePartial (fromJust (toEnum 2020)))
-    dayOfYear1 = (unsafePartial (fromJust (toEnum 1)))
-    dayOfYear365 = (unsafePartial (fromJust (toEnum 365)))
-    dayOfYear366 = (unsafePartial (fromJust (toEnum 366)))
+    year2018 = (Unsafe.unsafePartial (Maybe.fromJust (Enum.toEnum 2018)))
+    year2020 = (Unsafe.unsafePartial (Maybe.fromJust (Enum.toEnum 2020)))
+    dayOfYear1 = (Unsafe.unsafePartial (Maybe.fromJust (Enum.toEnum 1)))
+    dayOfYear365 = (Unsafe.unsafePartial (Maybe.fromJust (Enum.toEnum 365)))
+    dayOfYear366 = (Unsafe.unsafePartial (Maybe.fromJust (Enum.toEnum 366)))
   test "firstDayOfYear" do
-    Assert.equal dayOfYear1 (firstDayOfYear year2018)
+    Assert.equal dayOfYear1 (DayOfYear.firstDayOfYear year2018)
   test "lastDayOfYear" do
-    Assert.equal dayOfYear365 (lastDayOfYear year2018)
-    Assert.equal dayOfYear366 (lastDayOfYear year2020)
+    Assert.equal dayOfYear365 (DayOfYear.lastDayOfYear year2018)
+    Assert.equal dayOfYear366 (DayOfYear.lastDayOfYear year2020)
