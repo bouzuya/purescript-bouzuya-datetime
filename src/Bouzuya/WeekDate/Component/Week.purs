fea1d2bc1e06e989b56d7120d546b21fc30654bd
bouzuya <m@bouzuya.net>
2019-02-22 08:41:03 +0900

    remove unused vars

--- src/Bouzuya/WeekDate/Component/Week.purs
+++ src/Bouzuya/WeekDate/Component/Week.purs
@@ -2,17 +2,15 @@ module Bouzuya.WeekDate.Component.Week
   ( Week
   , firstWeekOfYear
   , lastWeekOfYear
-  , week
-  , weekYear
   ) where
 
-import Bouzuya.OrdinalDate as OrdinalDate
 import Bouzuya.WeekDate.Extra as WeekDateExtra
-import Data.Date (Date, Weekday(..), Year, exactDate, isLeapYear, weekday, year)
-import Data.Enum (class BoundedEnum, class Enum, Cardinality(..), fromEnum, pred, succ, toEnum)
-import Data.Maybe (Maybe(..), fromJust)
-import Partial.Unsafe (unsafePartial)
-import Prelude (class Bounded, class Eq, class Ord, class Show, bind, bottom, identity, join, negate, otherwise, pure, show, top, (&&), (*), (+), (-), (/), (<), (<$>), (<*>), (<<<), (<=), (<>), (==), (>), (>>=), (||))
+import Data.Date (Weekday(..), Year)
+import Data.Date as Date
+import Data.Enum (class BoundedEnum, class Enum, Cardinality(..))
+import Data.Enum as Enum
+import Data.Maybe (Maybe(..))
+import Prelude (class Bounded, class Eq, class Ord, class Show, bottom, otherwise, show, (&&), (+), (-), (<<<), (<=), (<>), (==), (||))
 
 data WeekDate = WeekDate Year Week Weekday
 
@@ -40,8 +38,8 @@ instance boundedEnumWeek :: BoundedEnum Week where
   fromEnum (Week n) = n
 
 instance enumWeek :: Enum Week where
-  succ = toEnum <<< (_ + 1) <<< fromEnum
-  pred = toEnum <<< (_ - 1) <<< fromEnum
+  succ = Enum.toEnum <<< (_ + 1) <<< Enum.fromEnum
+  pred = Enum.toEnum <<< (_ - 1) <<< Enum.fromEnum
 
 instance showWeek :: Show Week where
   show (Week n) = "(Week " <> show n <> ")"
@@ -53,66 +51,6 @@ lastWeekOfYear :: Year -> Week
 lastWeekOfYear y =
   let
     w = WeekDateExtra.firstWeekdayOfYear y
-    isLongYear = (w == Thursday) || (w == Wednesday && isLeapYear y)
+    isLongYear = (w == Thursday) || (w == Wednesday && Date.isLeapYear y)
   in
     if isLongYear then longYearLastWeek else shortYearLastWeek
-
-toDate :: WeekDate -> Date
-toDate (WeekDate y (Week w) d) =
-  let
-    dayOfYearOffset = case WeekDateExtra.firstWeekdayOfYear y of
-      Monday -> 0
-      Tuesday -> -1
-      Wednesday -> -2
-      Thursday -> -3
-      Friday -> 3
-      Saturday -> 2
-      Sunday -> 1
-    dayOfYear = (w - 1) * 7 + (fromEnum d) + dayOfYearOffset
-    dayOfYearTop y' = if isLeapYear y' then 366 else 365
-    ordinalDateMaybe =
-      if dayOfYear <= 0
-      then do
-        py <- pred y
-        doy <- toEnum ((dayOfYearTop py) + dayOfYear)
-        OrdinalDate.ordinalDate py doy
-      else if dayOfYear > dayOfYearTop y
-      then do
-        ny <- succ y
-        doy <- toEnum (dayOfYear - (dayOfYearTop y))
-        OrdinalDate.ordinalDate ny doy
-      else
-        OrdinalDate.ordinalDate <$> (pure y) <*> (toEnum dayOfYear) >>= identity
-    dateMaybe = OrdinalDate.toDate <$> ordinalDateMaybe
-  in
-    unsafePartial (fromJust dateMaybe)
-
-toWeekDate :: Date -> WeekDate
-toWeekDate d =
-  let
-    y = year d
-    w = weekday d
-    d0104 = unsafePartial (fromJust (join (exactDate y bottom <$> (toEnum 4))))
-    d1228 = unsafePartial (fromJust (join (exactDate y top <$> (toEnum 28))))
-    dayOfYear = OrdinalDate.dayOfYear <<< OrdinalDate.fromDate
-    doy = dayOfYear d
-  in
-    if doy < dayOfYear d0104 && weekday d > weekday d0104 then
-      let py = unsafePartial (fromJust (pred y)) -- FIXME
-      in WeekDate py (lastWeekOfYear py) w
-    else if doy > dayOfYear d1228 && weekday d < weekday d1228 then
-      let ny = unsafePartial (fromJust (succ y)) -- FIXME
-      in WeekDate ny (firstWeekOfYear ny) w
-    else
-      let woy = Week (((fromEnum doy) + (fromEnum (weekday d0104)) - 4 - 1) / 7 + 1)
-      in WeekDate y woy w
-
-week :: Date -> Week
-week d =
-  let (WeekDate _ w _) = toWeekDate d
-  in w
-
-weekYear :: Date -> Year
-weekYear d =
-  let (WeekDate y _ _) = toWeekDate d
-  in y
--- test/Bouzuya/WeekDate/Component/Week.purs
+++ test/Bouzuya/WeekDate/Component/Week.purs
@@ -1,6 +1,6 @@
 module Test.Bouzuya.WeekDate.Component.Week (tests) where
 
-import Bouzuya.WeekDate.Component.Week (Week, firstWeekOfYear, lastWeekOfYear, week, weekYear)
+import Bouzuya.WeekDate.Component.Week (Week, firstWeekOfYear, lastWeekOfYear)
 import Data.Date (Year)
 import Data.Date as Date
 import Data.Enum as Enum
@@ -40,75 +40,3 @@ tests = suite "Bouzuya.WeekDate.Component.Week" do
       , Tuple 53 2020 -- *leap year* & *Wed*
       ]
       \(Tuple w y) -> Assert.equal (unsafeWeek w) (lastWeekOfYear (unsafeYear y))
-  test "week" do
-    -- --01-04/--12-28
-    Assert.equal (unsafeWeek 1) (week (unsafeDate 2004 1 4))
-    Assert.equal (unsafeWeek 2) (week (unsafeDate 2004 1 5))
-    Assert.equal (unsafeWeek 53) (week (unsafeDate 2004 12 27))
-    Assert.equal (unsafeWeek 53) (week (unsafeDate 2004 12 28))
-    -- Mon -> 1 Curr
-    Assert.equal (unsafeWeek 52) (week (unsafeDate 2000 12 31)) -- 2000-W52-7
-    Assert.equal (unsafeWeek 1) (week (unsafeDate 2001 1 1)) -- 2001-W01-1 (Mon)
-    -- Tue -> 1 Curr
-    Assert.equal (unsafeWeek 52) (week (unsafeDate 2001 12 30)) -- 2001-W52-7
-    Assert.equal (unsafeWeek 1) (week (unsafeDate 2001 12 31)) -- 2002-W01-1
-    Assert.equal (unsafeWeek 1) (week (unsafeDate 2002 1 1)) -- 2002-W01-2 (Tue)
-    -- Wed -> 1 Curr
-    Assert.equal (unsafeWeek 52) (week (unsafeDate 2002 12 29)) -- 2002-W52-7
-    Assert.equal (unsafeWeek 1) (week (unsafeDate 2002 12 31)) -- 2003-W01-2
-    Assert.equal (unsafeWeek 1) (week (unsafeDate 2003 1 1)) -- 2003-W01-3 (Wed)
-    -- Thu -> 1 Curr
-    Assert.equal (unsafeWeek 52) (week (unsafeDate 2003 12 28)) -- 2003-W52-7
-    Assert.equal (unsafeWeek 1) (week (unsafeDate 2003 12 31)) -- 2004-W01-3
-    Assert.equal (unsafeWeek 1) (week (unsafeDate 2004 1 1)) -- 2004-W01-4 (Thu)
-    -- Fri -> 53 Prev
-    Assert.equal (unsafeWeek 53) (week (unsafeDate 2009 12 31)) -- 2009-W53-4
-    Assert.equal (unsafeWeek 53) (week (unsafeDate 2010 1 1)) -- 2009-W53-5 (Fri)
-    Assert.equal (unsafeWeek 1) (week (unsafeDate 2010 1 4)) -- 2010-W01-1
-    -- Sat -> 53 Prev (prev year is leap year)
-    Assert.equal (unsafeWeek 53) (week (unsafeDate 2004 12 31)) -- 2004-W53-5
-    Assert.equal (unsafeWeek 53) (week (unsafeDate 2005 1 1)) -- 2004-W53-6 (Sat)
-    Assert.equal (unsafeWeek 1) (week (unsafeDate 2005 1 3)) -- 2005-W01-1
-    -- Sat -> 52 Prev (prev year is not leap year)
-    Assert.equal (unsafeWeek 52) (week (unsafeDate 1999 12 31)) -- 1999-W52-5
-    Assert.equal (unsafeWeek 52) (week (unsafeDate 2000 1 1)) -- 1999-W52-6 (Sat)
-    Assert.equal (unsafeWeek 1) (week (unsafeDate 2000 1 3)) -- 2000-W01-1
-    -- Sun -> 52 Prev
-    Assert.equal (unsafeWeek 52) (week (unsafeDate 2005 12 31)) -- 2005-W52-6
-    Assert.equal (unsafeWeek 52) (week (unsafeDate 2006 1 1)) -- 2005-W52-7 (Sun)
-  test "weekYear" do
-    -- --01-04/--12-28
-    Assert.equal (unsafeYear 2004) (weekYear (unsafeDate 2004 1 4))
-    Assert.equal (unsafeYear 2004) (weekYear (unsafeDate 2004 1 5))
-    Assert.equal (unsafeYear 2004) (weekYear (unsafeDate 2004 12 27))
-    Assert.equal (unsafeYear 2004) (weekYear (unsafeDate 2004 12 28))
-    -- Mon -> 1 Curr
-    Assert.equal (unsafeYear 2000) (weekYear (unsafeDate 2000 12 31)) -- 2000-W52-7
-    Assert.equal (unsafeYear 2001) (weekYear (unsafeDate 2001 1 1)) -- 2001-W01-1 (Mon)
-    -- Tue -> 1 Curr
-    Assert.equal (unsafeYear 2001) (weekYear (unsafeDate 2001 12 30)) -- 2001-W52-7
-    Assert.equal (unsafeYear 2002) (weekYear (unsafeDate 2001 12 31)) -- 2002-W01-1
-    Assert.equal (unsafeYear 2002) (weekYear (unsafeDate 2002 1 1)) -- 2002-W01-2 (Tue)
-    -- Wed -> 1 Curr
-    Assert.equal (unsafeYear 2002) (weekYear (unsafeDate 2002 12 29)) -- 2002-W52-7
-    Assert.equal (unsafeYear 2003) (weekYear (unsafeDate 2002 12 31)) -- 2003-W01-2
-    Assert.equal (unsafeYear 2003) (weekYear (unsafeDate 2003 1 1)) -- 2003-W01-3 (Wed)
-    -- Thu -> 1 Curr
-    Assert.equal (unsafeYear 2003) (weekYear (unsafeDate 2003 12 28)) -- 2003-W52-7
-    Assert.equal (unsafeYear 2004) (weekYear (unsafeDate 2003 12 31)) -- 2004-W01-3
-    Assert.equal (unsafeYear 2004) (weekYear (unsafeDate 2004 1 1)) -- 2004-W01-4 (Thu)
-    -- Fri -> 53 Prev
-    Assert.equal (unsafeYear 2009) (weekYear (unsafeDate 2009 12 31)) -- 2009-W53-4
-    Assert.equal (unsafeYear 2009) (weekYear (unsafeDate 2010 1 1)) -- 2009-W53-5 (Fri)
-    Assert.equal (unsafeYear 2010) (weekYear (unsafeDate 2010 1 4)) -- 2010-W01-1
-    -- Sat -> 53 Prev (prev year is leap year)
-    Assert.equal (unsafeYear 2004) (weekYear (unsafeDate 2004 12 31)) -- 2004-W53-5
-    Assert.equal (unsafeYear 2004) (weekYear (unsafeDate 2005 1 1)) -- 2004-W53-6 (Sat)
-    Assert.equal (unsafeYear 2005) (weekYear (unsafeDate 2005 1 3)) -- 2005-W01-1
-    -- Sat -> 52 Prev (prev year is not leap year)
-    Assert.equal (unsafeYear 1999) (weekYear (unsafeDate 1999 12 31)) -- 1999-W52-5
-    Assert.equal (unsafeYear 1999) (weekYear (unsafeDate 2000 1 1)) -- 1999-W52-6 (Sat)
-    Assert.equal (unsafeYear 2000) (weekYear (unsafeDate 2000 1 3)) -- 2000-W01-1
-    -- Sun -> 52 Prev
-    Assert.equal (unsafeYear 2005) (weekYear (unsafeDate 2005 12 31)) -- 2005-W52-6
-    Assert.equal (unsafeYear 2005) (weekYear (unsafeDate 2006 1 1)) -- 2005-W52-7 (Sun)
