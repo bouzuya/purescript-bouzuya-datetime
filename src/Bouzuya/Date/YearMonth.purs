8e97741ffc9925b24254d61ff43da5d07ded0ea7
bouzuya <m@bouzuya.net>
2019-02-22 23:25:50 +0900

    add {first,last}{Date,Day}OfMonth

--- src/Bouzuya/Date/YearMonth.purs
+++ src/Bouzuya/Date/YearMonth.purs
@@ -1,6 +1,10 @@
 module Bouzuya.Date.YearMonth
   ( YearMonth
+  , firstDateOfMonth
+  , firstDayOfMonth
   , fromDate
+  , lastDateOfMonth
+  , lastDayOfMonth
   , toDate
   , yearMonth
   ) where
@@ -8,11 +12,29 @@ module Bouzuya.Date.YearMonth
 import Data.Date (Date, Day, Month, Year)
 import Data.Date as Date
 import Data.Maybe (Maybe)
+import Data.Maybe as Maybe
+import Partial.Unsafe as Unsafe
+import Prelude (bottom)
 
 data YearMonth = YearMonth Year Month
 
+firstDateOfMonth :: YearMonth -> Date
+firstDateOfMonth (YearMonth y m) =
+  Unsafe.unsafePartial (Maybe.fromJust (Date.exactDate y m bottom))
+
+firstDayOfMonth :: YearMonth -> Day
+firstDayOfMonth _ = bottom
+
 fromDate :: Date -> YearMonth
 fromDate date = YearMonth (Date.year date) (Date.month date)
+
+lastDateOfMonth :: YearMonth -> Date
+lastDateOfMonth (YearMonth y m) =
+  Unsafe.unsafePartial
+    (Maybe.fromJust (Date.exactDate y m (Date.lastDayOfMonth y m)))
+
+lastDayOfMonth :: YearMonth -> Day
+lastDayOfMonth (YearMonth y m) = Date.lastDayOfMonth y m
 
 toDate :: Day -> YearMonth -> Maybe Date
 toDate day (YearMonth y m) = Date.exactDate y m day
--- test/Bouzuya/Date/YearMonth.purs
+++ test/Bouzuya/Date/YearMonth.purs
@@ -3,6 +3,7 @@ module Test.Bouzuya.Date.YearMonth
   ) where
 
 import Bouzuya.Date.YearMonth as YearMonth
+import Data.Date (Day)
 import Data.Date as Date
 import Data.Enum as Enum
 import Data.Maybe (Maybe(..))
@@ -14,6 +15,30 @@ import Test.Unit.Assert as Assert
 
 tests :: TestSuite
 tests = suite "Bouzuya.Date.YearMonth" do
+  test "firstDateOfMonth" do
+    Assert.equal
+      do
+        y <- Enum.toEnum 2000
+        m <- Enum.toEnum 1
+        d <- Enum.toEnum 1
+        Date.exactDate y m d
+      do
+        y <- Enum.toEnum 2000
+        m <- Enum.toEnum 1
+        d <- Enum.toEnum 2
+        date <- Date.exactDate y m d
+        pure (YearMonth.firstDateOfMonth (YearMonth.fromDate date))
+
+  test "firstDayOfMonth" do
+    Assert.equal
+      ((Enum.toEnum 1) :: Maybe Day) -- --01
+      do
+        y <- Enum.toEnum 2000
+        m <- Enum.toEnum 1
+        d <- Enum.toEnum 2
+        date <- Date.exactDate y m d
+        pure (YearMonth.firstDayOfMonth (YearMonth.fromDate date))
+
   test "fromDate / toDate" do
     let
       d1Maybe = do
@@ -25,6 +50,38 @@ tests = suite "Bouzuya.Date.YearMonth" do
     Assert.equal
       (Just d1)
       (YearMonth.toDate (Date.day d1) (YearMonth.fromDate d1))
+
+  test "lastDateOfMonth" do
+    Assert.equal
+      do
+        y <- Enum.toEnum 2000
+        m <- Enum.toEnum 1
+        d <- Enum.toEnum 31
+        Date.exactDate y m d
+      do
+        y <- Enum.toEnum 2000
+        m <- Enum.toEnum 1
+        d <- Enum.toEnum 2
+        date <- Date.exactDate y m d
+        pure (YearMonth.lastDateOfMonth (YearMonth.fromDate date))
+
+  test "lastDayOfMonth" do
+    Assert.equal
+      ((Enum.toEnum 31) :: Maybe Day) -- --31
+      do
+        y <- Enum.toEnum 2000
+        m <- Enum.toEnum 1
+        d <- Enum.toEnum 2
+        date <- Date.exactDate y m d
+        pure (YearMonth.lastDayOfMonth (YearMonth.fromDate date))
+    Assert.equal
+      ((Enum.toEnum 29) :: Maybe Day) -- --29 (2000 is leap year)
+      do
+        y <- Enum.toEnum 2000
+        m <- Enum.toEnum 2
+        d <- Enum.toEnum 1
+        date <- Date.exactDate y m d
+        pure (YearMonth.lastDayOfMonth (YearMonth.fromDate date))
 
   test "yearMonth" do
     y <- pure (Enum.toEnum 2000)
