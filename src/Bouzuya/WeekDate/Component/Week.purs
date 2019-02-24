18145036a25a3dd7b67a92d19880fe7a2de903cf
bouzuya <m@bouzuya.net>
2019-02-24 11:46:08 +0900

    add tests for Week

--- src/Bouzuya/WeekDate/Component/Week.purs
+++ src/Bouzuya/WeekDate/Component/Week.purs
@@ -11,24 +11,24 @@ import Prelude (class Bounded, class Eq, class Ord, class Show, otherwise, show,
 
 newtype Week = Week Int
 
-derive newtype instance eqWeek :: Eq Week
-
-derive newtype instance ordWeek :: Ord Week
-
 instance boundedWeek :: Bounded Week where
   bottom = Week 1
   top = Week 53
 
 instance boundedEnumWeek :: BoundedEnum Week where
   cardinality = Enum.Cardinality 53
+  fromEnum (Week n) = n
   toEnum n
     | 1 <= n && n <= 53 = Just (Week n)
     | otherwise = Nothing
-  fromEnum (Week n) = n
 
 instance enumWeek :: Enum Week where
-  succ = Enum.toEnum <<< (_ + 1) <<< Enum.fromEnum
   pred = Enum.toEnum <<< (_ - 1) <<< Enum.fromEnum
+  succ = Enum.toEnum <<< (_ + 1) <<< Enum.fromEnum
+
+derive newtype instance eqWeek :: Eq Week
+
+derive newtype instance ordWeek :: Ord Week
 
 instance showWeek :: Show Week where
   show (Week n) = "(Week " <> show n <> ")"
--- test/Bouzuya/WeekDate/Component/Week.purs
+++ test/Bouzuya/WeekDate/Component/Week.purs
@@ -1,8 +1,48 @@
 module Test.Bouzuya.WeekDate.Component.Week (tests) where
 
-import Prelude (pure, unit)
-import Test.Unit (TestSuite, suite)
+import Bouzuya.WeekDate (Week)
+import Bouzuya.WeekDate.Component.Week as Week
+import Data.Enum as Enum
+import Data.Maybe (Maybe(..))
+import Prelude (bottom, discard, pure, show, top, unit, (<))
+import Test.Unit (TestSuite, suite, test)
+import Test.Unit.Assert as Assert
 
 tests :: TestSuite
 tests = suite "Bouzuya.WeekDate.Component.Week" do
-  pure unit -- TODO
+  test "Bounded Week" do
+    Assert.equal ((Enum.toEnum 1) :: _ Week) (Just (bottom :: Week))
+    Assert.equal ((Enum.toEnum 53) :: _ Week) (Just (top :: Week))
+
+  test "BoundedEnum Week" do
+    let (Enum.Cardinality n) = Enum.cardinality :: _ Week
+    Assert.equal 1 (Enum.fromEnum (bottom :: Week))
+    Assert.equal 53 (Enum.fromEnum (top :: Week))
+    Assert.equal Nothing ((Enum.toEnum 0) :: _ Week)
+    Assert.equal (Just (bottom :: Week)) ((Enum.toEnum 1) :: _ Week)
+    Assert.equal (Just (top :: Week)) ((Enum.toEnum 53) :: _ Week)
+    Assert.equal Nothing ((Enum.toEnum 54) :: _ Week)
+
+  test "Enum Week" do
+    Assert.equal Nothing (Enum.pred (bottom :: Week))
+    Assert.equal Nothing (Enum.succ (top :: Week))
+    Assert.equal (Enum.toEnum 52) (Enum.pred (top :: Week))
+    Assert.equal (Enum.toEnum 2) (Enum.succ (bottom :: Week))
+
+  test "Eq Week" do
+    pure unit
+
+  test "Ord Week" do
+    Assert.assert
+      "(Week 1) < (Week 53)"
+      ((bottom :: Week) < (top :: Week))
+
+  test "Show Week" do
+    Assert.equal "(Week 1)" (show (bottom :: Week))
+    Assert.equal "(Week 53)" (show (top :: Week))
+
+  test "lastWeekOfLongYear" do
+    Assert.equal "(Week 53)" (show (Week.lastWeekOfLongYear))
+
+  test "lastWeekOfShortYear" do
+    Assert.equal "(Week 52)" (show (Week.lastWeekOfShortYear))
